ba2dd3abe8cbbd98a94abc59b5e8d219586d7541
assalloca <hugo.assal1@etu.univ-lorraine.fr>
2023-11-06 17:21:40 +0100

    changement mineurs

--- SAE/index.php
+++ SAE/index.php
@@ -7,16 +7,19 @@
 <body>
 
 <link rel='stylesheet' href='css.css'>
-    <div class='Entete'>
-        <img src="img/Logo.png" alt="Le logo principal"/>
-    </div>
-    <div class='Entete'>ACCUEIL</div>
-        <form>
-         <div>
-             <label for="maRecherche"></label><input type = "search" id="maRecherche" name="q" placeholder="Chercher"/>
-         </div>
-        </form>
+<div class='Entete'>
+    <img src="img/Logo.png" alt="Le logo principal"/>
+</div>
+<div class='Entete'>ACCUEIL</div>
+<form>
+ <div>
+     <input type = "search" id="maRecherche" name="q" placeholder="Chercher"/>
+ </div>
+</form>
+<div class = "Connexion">
+    <a href="connexion.php">Connexion</a>
+</div>
+
 
 </body>
 </html>
-
