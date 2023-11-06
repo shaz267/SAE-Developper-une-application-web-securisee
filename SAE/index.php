d5c25e771c44dc2859bd0d64d4954a139d3c670f
Marin <bourdonmarin@gmail.com>
2023-11-06 15:40:40 +0100

    modification page d'accueil

--- SAE/index.php
+++ SAE/index.php
@@ -1,8 +1,11 @@
 <?php
     echo "<html>
+    <link rel='stylesheet' href='css.css'>
+    <div class='tete_page'>
     <div class='connexion'>Connexion/Profil</div>
-    <div class='?'>JSP ENCORE</div>
+    <div class='bb'>JSP ENCORE</div>
     <div class='recherche'>Barre de recherche</div>
+    </div>
     <body>
     </body>
     </html>";
