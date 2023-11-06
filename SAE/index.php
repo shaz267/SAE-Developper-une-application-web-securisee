b5e0ff9177e8597a7210a984d636e7bd8232872e
Marin <bourdonmarin@gmail.com>
2023-11-06 15:22:48 +0100

    ajout du projet

---
+++ SAE/index.php
@@ -0,0 +1,8 @@
+<?php
+    echo "<html>
+    <div class='connexion'>Connexion/Profil</div>
+    <div class='?'>JSP ENCORE</div>
+    <div class='recherche'>Barre de recherche</div>
+    <body>
+    </body>
+    </html>";
