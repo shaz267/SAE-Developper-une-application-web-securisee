2656b4a22db97e9862eff127cf8fdfce84ad54ee
matheo <matheo.grandjean8@etu.univ-lorraine.fr>
2023-11-06 15:58:36 +0100

    réparation du fichier html

--- SAE/index.php
+++ SAE/index.php
@@ -1,11 +1,19 @@
+<html lang="fr">
+
+<head>
+    <title>Page d'accueil</title>
+    <meta charset="utf-8">
+<body>
+
 <?php
-    echo "<html>
-    <link rel='stylesheet' href='css.css'>
+
+echo "<link rel='stylesheet' href='css.css'>
     <div class='tete_page'>
     <div class='connexion'>Touiter</div>
     <div class='bb'>ACCUEIL Quoi de neuf? Publier</div>
     <div class='recherche'>Barre de recherche</div>
-    </div>
-    <body>
-    </body>
-    </html>";
+    </div>"
+?>
+
+</body>
+</html>
