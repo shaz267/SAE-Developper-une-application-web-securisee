79dcff56adcd219fd3a4a0c24fa1319ecd76340d
Marin <bourdonmarin@gmail.com>
2023-11-06 17:23:32 +0100

    modif css

--- SAE/index.php
+++ SAE/index.php
@@ -7,16 +7,16 @@
 <body>
 
 <link rel='stylesheet' href='css.css'>
-    <div class='Entete'>
-        <img src="img/Logo.png" alt="Le logo principal"/>
+    <div class='container'>
+        <div class='logo'>
+            <img src="img/Logo.png" alt="Le logo principal"/>
+        </div>
+        <div class='accueil'>ACCUEIL</div>
+        <div class="recherche">
+            <label>Que cherchez vous?</label>
+            <input type="search" placeholder="Chercher"/>
+        </div>
     </div>
-    <div class='Entete'>ACCUEIL</div>
-        <form>
-         <div>
-             <label for="maRecherche"></label><input type = "search" id="maRecherche" name="q" placeholder="Chercher"/>
-         </div>
-        </form>
-
 </body>
 </html>
 
