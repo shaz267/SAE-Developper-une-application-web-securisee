7867515f297ba090a05ed0cb93ebd1d57d110ebd
assalloca <hugo.assal1@etu.univ-lorraine.fr>
2023-11-07 15:09:04 +0100

    Légers changements

--- SAE/src/Action/TouitesAction.php
+++ SAE/src/Action/TouitesAction.php
@@ -8,6 +8,21 @@ class TouitesAction extends Action
     public function execute(): string
     {
 
-        return 'TouitesAction';
+        return  <<<HTML
+                 <div class="liens">
+                     <ul id="choix">
+                         <li><a href="?action=index.php">Accueil</a></li>
+                         <li><a href="profil.php">Profil</a></li>
+                         <li><a href="parametre.php">Parametre</a></li>
+                         <li><a href="deconnexion.php">Connexion</a></li>
+                     </ul>
+                 </div>
+                 <div class="deffilementTouite">
+                     <div class="touite">
+                         
+                     </div>
+                 </div>
+                 HTML;
+
     }
 }
