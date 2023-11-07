909a79868fa5f9183b467e406fa0e14aa2285e7f
assalloca <hugo.assal1@etu.univ-lorraine.fr>
2023-11-07 17:25:44 +0100

    Connexion réussi

--- SAE/index.php
+++ SAE/index.php
@@ -4,7 +4,7 @@ require_once 'vendor\autoload.php';
 
 use touiteur\classes\ConnectionFactory;
 
-ConnectionFactory::setConfig();
+ConnectionFactory::setConfig('db_config.ini');
 
 $pdo = ConnectionFactory::makeConnection();
 
