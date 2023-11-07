d6d68fc3123247daf82888dcce7e51271e64ff7b
assalloca <hugo.assal1@etu.univ-lorraine.fr>
2023-11-07 15:29:04 +0100

    Changement TouitesAction.php

--- SAE/src/classes/Action/TouitesAction.php
+++ SAE/src/classes/Action/TouitesAction.php
@@ -13,7 +13,7 @@ class TouitesAction extends Action
                      <ul id="choix">
                          <li><a href="?action=TouitesAction">Accueil</a></li>
                          <li><a href="?action=Connexion">Connexion</a></li>
-                         <li><a href="?action=">Inscription</a></li>
+                         <li><a href="?action=Inscription">Inscription</a></li>
                      </ul>
                  </div>
                  <div class="deffilementTouite">
