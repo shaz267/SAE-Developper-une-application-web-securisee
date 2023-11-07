37b5b8ad667206c3dc72c4b0438120ef00ecf6e3
assalloca <hugo.assal1@etu.univ-lorraine.fr>
2023-11-07 15:39:13 +0100

    Modifications Dispatcher.php

--- SAE/src/classes/Dispatcher.php
+++ SAE/src/classes/Dispatcher.php
@@ -1,9 +1,9 @@
 <?php
+declare(strict_types=1);
 
 namespace src\classes;
 
-use DefaultAction;
-use SAE\src\classes\TouitesAction;
+use \src\classes\ConnectionFactory;
 
 require_once 'vendor/autoload.php';
 
@@ -15,10 +15,15 @@ class Dispatcher
 {
 
     private $action;
+    private $html;
 
     public function __construct()
     {
-        $this->action = $_GET['action'];
+        if (isset($_GET["action"]))
+            $this->action = $_GET["action"];
+        else
+            $this->action = "";
+        $this->html = "";
     }
 
     public function run()
@@ -26,12 +31,13 @@ class Dispatcher
 
         switch ($this->action) {
             case 'TouitesAction':
-                $html = $this->executeAction(new TouitesAction());
-                $this->renderPage($html);
+                $tA = new TouitesAction();
+                $this->html = $tA->execute();
+                $this->renderPage($this->html);
                 break;
             default:
-                $html = $this->executeAction(new DefaultAction());
-                $this->renderPage($html);
+
+                $this->renderPage($this->html);
 
         }
     }
@@ -51,7 +57,7 @@ class Dispatcher
                     <link rel='stylesheet' href='css.css'>
                     <div class='container'>
                         <div class='logo'>
-                            <a href="index.php">
+                            <a href="?action=TouitesAction">
                                 <img src="img/logo.png" alt="Le logo principal"/>
                             </a>
                         </div>
@@ -70,41 +76,7 @@ class Dispatcher
                 <section>
                     <div class="touites">
                 
-                        <div class="liens">
-                            <ul id="choix">
-                                <li><a href="index.php">Accueil</a></li>
-                                <li><a href="profil.php">Profil</a></li>
-                                <li><a href="parametre.php">Parametre</a></li>
-                                <li><a href="deconnexion.php">Deconnexion</a></li>
-                            </ul>
-                        </div>
-                        <div class="deffilementTouite">
-                            <div class="touite">
-                                <h1>Profil du touitos</h1>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel dui metus. Sed
-                                facilisis nunc a felis placerat, in venenatis quam placerat. Sed fringilla mauris id eros
-                                consectetur, a tincidunt nunc venenatis. Nulla leo nunc, vestibulum eu justo non, viverra tincidunt
-                                mi.
-                                Nullam cursus eget ligula vel finibus.
-                                Ut sed facilisis nulla, sed placerat velit. Integer rhoncus felis dolor, quis interdum tellus
-                                finibus ut.
-                                Aenean convallis feugiat magna, at tincidunt libero varius quis. Nullam semper lorem velit, a
-                                finibus metus fringilla ac. Vestibulum semper varius leo, ac elementum elit blandit pulvinar. Donec
-                                tempus fermentum elit a posuere. Fusce volutpat ipsum eget nisl suscipit tempus. Phasellus finibus
-                                porttitor porta.
-                                Nulla eleifend sem a nulla placerat ultricies. Nulla sed nunc commodo, convallis lacus ac, viverra
-                                turpis. Vivamus congue nisl eget turpis dignissim bibendum. Vivamus ac volutpat velit.
-                                Quisque a justo orci. Aenean varius mattis nisi condimentum ultrices. Vestibulum faucibus quis ex
-                                sed aliquam. Vivamus in mauris eu leo vestibulum auctor ullamcorper nec orci. Ut mollis hendrerit
-                                cursus.
-                                Quisque at ultrices purus, eget blandit felis. Aenean quis fermentum augue. Praesent iaculis dui eu
-                                dolor pharetra, scelerisque lobortis lorem accumsan. Nulla laoreet, magna sit amet placerat
-                                faucibus, nibh sem ullamcorper nibh, eu eleifend ipsum tortor sed dui.
-                                Aliquam erat volutpat. Aenean ut nisl pulvinar, venenatis diam et,
-                                volutpat justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-                                mus. Nunc in aliquet ex, ac rhoncus elit. Donec.
-                            </div>
-                        </div>
+                        $this->html
                     </div>
                     <aside>
                         <div>
