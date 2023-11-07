2eb1b3640ed4b171a0cf79700150e05b076a5cee
assalloca <hugo.assal1@etu.univ-lorraine.fr>
2023-11-07 14:11:45 +0100

    Modifications de Dispatcher.php

---
+++ SAE/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '158def614de1c10c15e1ba427f77c088f3b2bafd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '158def614de1c10c15e1ba427f77c088f3b2bafd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
