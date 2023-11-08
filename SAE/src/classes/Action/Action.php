c2ef89a326ac6af39ba8cc2af8f1e5bdc87adad7
assalloca <hugo.assal1@etu.univ-lorraine.fr>
2023-11-08 14:35:05 +0100

    Factorisation du code

--- SAE/src/classes/Action/Action.php
+++ SAE/src/classes/Action/Action.php
@@ -18,4 +18,37 @@ abstract class Action
     }
 
     abstract public function execute(): string;
+
+    /**
+     * Fonction qui permet de retourner le code HTML pour afficher les touites
+     * @param array $touites
+     * @return string
+     */
+    public static function renderTouites(array $touites): string
+    {
+
+        $html = "";
+        //On parcourt les touites
+        foreach ($touites as $touite) {
+
+            //On convertit le contenu en UTF-8
+            $touite['contenu'] = utf8_encode($touite['contenu']);
+
+            //On réduit le contenu pour l'afficher en version courte
+            $touite['contenu'] = substr($touite['contenu'], 0, 40) . ' ...';
+
+            $html .= <<<HTML
+            <div class="touite" onclick="location.href='?action=TouiteDetailAction&touite_id={$touite['id_touite']}'">
+                <a href="?action=TouitesPersonneAction&id={$touite['id_user']}"><h3>{$touite['nom']} {$touite['prenom']}</h3></a>
+                <br>
+                <p>{$touite['contenu']}</p>
+                <br>
+                <p>Date du post : {$touite['date_pub']}</p>
+                <br>
+            </div>
+            HTML;
+        }
+        //On retourne le code HTML
+        return $html;
+    }
 }
