d4ccc210d8ca2a85e0ef1dcbc5daed021e62dd7c
Marin <bourdonmarin@gmail.com>
2023-11-08 15:46:44 +0100

    Ajout lien Inscription

--- SAE/src/classes/Action/InscriptionAction.php
+++ SAE/src/classes/Action/InscriptionAction.php
@@ -2,9 +2,11 @@
 
 namespace touiteur\classes\Action;
 
+use PDO;
 use touiteur\classes\ConnectionFactory;
 
 class InscriptionAction extends Action {
+
 
     public function execute(): string
     {
@@ -22,14 +24,20 @@ class InscriptionAction extends Action {
         $html = $this->renderTouites($touites);*/
 
         $html = <<<HTML
-                 <form id="inscription" method="POST" action="?action=Inscription">
+                 <form id="inscription" method="POST" action="?action=InscriptionAction">
                  <label>Votre Nom : </label><input type="text" name="nom"><br>
                  <label>Votre Prénom : </label><input type="text" name="prenom"><br>
                  <label>Votre e-mail : </label><input type="text" name="email"><br>
-                 <label>Votre mot de passe : </label><input type="text" name="mdp">
+                 <label>Votre mot de passe : </label><input type="password" name="mdp"><br>
+                 <input type="submit" name="valider" class="button" value="Valider"/>
                  </form>
                  HTML;
-
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $this->register();
+        }
+        elseif($_SERVER['REQUEST_METHOD'] == 'GET'){
+            //afficher le mur
+        }
         return  <<<HTML
                  <div class="touites" id="index">
                  <div class="liens">
@@ -46,5 +54,25 @@ class InscriptionAction extends Action {
                  </div>
                  </div>
                  HTML;
+    }
+
+    public static function register(){
+
+        $nom = htmlspecialchars($_POST['nom']);
+        $prenom = htmlspecialchars($_POST['prenom']);
+        $mail = htmlspecialchars($_POST['email']);
+        $mdp = htmlspecialchars($_POST['mdp']);
+
+        $db = ConnectionFactory::makeConnection();
+
+        $hash=password_hash($mdp, PASSWORD_DEFAULT, ['cost'=> 12] );
+
+        $query = 'INSERT INTO Utilisateur (nom, prenom, email, mdp) VALUES (?, ?, ?, ?)';
+        $st = $db->prepare($query);
+        $st->bindParam(1, $nom, PDO::PARAM_STR);
+        $st->bindParam(2, $prenom, PDO::PARAM_STR);
+        $st->bindParam(3, $mail, PDO::PARAM_STR);
+        $st->bindParam(4, $hash, PDO::PARAM_STR);
+        $st->execute();
     }
 }
