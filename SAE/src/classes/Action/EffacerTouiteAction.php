95ba52015745da56201366d3283bd9e3451b174c
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2023-11-09 15:45:33 +0100

    Ajout possibilité de supprimer des touites avec des # dedans

--- SAE/src/classes/Action/EffacerTouiteAction.php
+++ SAE/src/classes/Action/EffacerTouiteAction.php
@@ -20,12 +20,25 @@ class EffacerTouiteAction extends Action
             // On se connecte à la base de données
             $pdo = ConnectionFactory::makeConnection();
 
-            // On exécute la requête SQL
-            $sql = "DELETE FROM touite WHERE id_touite = ?";
+            $sql = "SELECT id_tag FROM TOUITETAG WHERE id_touite = ?";
             $stmt = $pdo->prepare($sql);
             $stmt->bindParam(1, $touiteId, PDO::PARAM_INT);
             $stmt->execute();
-
+            $touite = $stmt->fetchAll();
+            if($stmt->rowCount() != 0){
+                foreach($touite as $tag){
+                    $sql = "DELETE FROM TOUITETAG WHERE id_tag = ?";
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->bindParam(1,$tag['id_tag'],PDO::PARAM_INT);
+                    $stmt->execute();
+                }
+            } else {
+                // On exécute la requête SQL
+                $sql = "DELETE FROM touite WHERE id_touite = ?";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(1, $touiteId, PDO::PARAM_INT);
+                $stmt->execute();
+            }
             // On redirige l'utilisateur vers la page d'accueil
             header("Location: ?action=MurAction");
         }
